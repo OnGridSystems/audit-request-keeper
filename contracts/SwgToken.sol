ca5a68ff52ae57be511f7d8f50511c4c1de3d522
Kirill Varlamov <kirill@ongrid.pro>
2019-04-13 13:53:18 +0300

    SKYSC-37 Create separate repo

---
+++ contracts/SwgToken.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+import "./Claimable.sol";
+import "../openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+
+
+contract ProjectToken is ERC20, ERC20Burnable, Claimable {
+    string public name = "SkyWay Global Token";
+    string public symbol = "SWG";
+    uint8 public decimals = 8;
+
+    /**
+     * @dev Function to mint tokens
+     * @param to The address that will receive the minted tokens.
+     * @param value The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address to, uint256 value) public onlyOwner returns (bool) {
+        _mint(to, value);
+        return true;
+    }
+}
